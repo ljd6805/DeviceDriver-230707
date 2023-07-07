15e2cffccca3eda9be1e725618c41aabdef21adf
jdeok <ljd6805@gmail.com>
2023-07-07 11:16:09 +0900

    Device driver Read 커밋

--- Project10/DeviceDriver.cpp
+++ Project10/DeviceDriver.cpp
@@ -1,4 +1,17 @@
 #include "DeviceDriver.h"
+#include <stdexcept>
+#include <windows.h>
+
+class READException : public std::exception
+{
+public:
+    explicit READException(char const* Messeage)
+	: exception(Messeage)
+    {
+	    
+    }
+};
+
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
@@ -6,7 +19,19 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int value = 0;
+
+    value = m_hardware->read(address);
+    for(int index=0;index<4;index++)
+    {
+        Sleep(200);
+        if(value != m_hardware->read(address))
+        {
+            throw READException("wrongReadValue");
+        }
+    }
+
+    return value;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/DeviceDriver-test.cpp
+++ Sample-Test1/DeviceDriver-test.cpp
@@ -1,10 +1,45 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-#include "../Project10/DeviceDriver.h"
+#include "../Project10/DeviceDriver.cpp"
 using namespace testing;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-  EXPECT_THAT(1, 1);
+class MockFlashMemory:public FlashMemoryDevice
+{
+public:
+	//unsigned char read(long address) override;
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void , write, (long address, unsigned char data), (override));
+};
+
+TEST(TestDeviceDriver, Driver_ReadOk) {
+	MockFlashMemory mock;
+
+	EXPECT_CALL(mock, read)
+		.Times(5)
+		.WillRepeatedly(Return((unsigned char)0xff));
+
+	DeviceDriver testdriver(&mock);
+
+	long inputAddress = 0x1234;
+	int expected = 0xff;
+	int actual = testdriver.read(inputAddress);
+
+	EXPECT_EQ(expected, actual);
+
+}
+
+TEST(TestDeviceDriver, Driver_ReadException) {
+	MockFlashMemory mock;
+
+	EXPECT_CALL(mock, read)
+		.WillOnce(Return((unsigned char)0xf1))
+		.WillOnce(Return((unsigned char)0xf1))
+		.WillRepeatedly(Return((unsigned char)0xff));
+		
+
+	DeviceDriver testdriver(&mock);
+	long inputAddress = 0x1234;
+	
+	EXPECT_THROW(testdriver.read(inputAddress), READException);
+
 }
