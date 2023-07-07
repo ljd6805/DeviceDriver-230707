00027e9b8d0e04e85b1a9384c8a264b00b49b67d
jdeok <ljd6805@gmail.com>
2023-07-07 10:10:58 +0900

    첫번째 commit

---
+++ Sample-Test1/DeviceDriver-test.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../Project10/DeviceDriver.h"
+using namespace testing;
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+  EXPECT_THAT(1, 1);
+}
